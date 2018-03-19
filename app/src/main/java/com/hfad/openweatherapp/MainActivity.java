a2f638e58dd9331c62faabf5bd621218578b1755
donald <donaldjames.cull@my.jcu.edu.au>
2018-03-19 17:54:47 +1000

    weather data is formatted and presented to user in constraint layout

--- app/src/main/java/com/hfad/openweatherapp/MainActivity.java
+++ app/src/main/java/com/hfad/openweatherapp/MainActivity.java
@@ -2,56 +2,86 @@ package com.hfad.openweatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    Weather weather;
     TextView textView;
     String city = "Townsville";
     String weatherFormat = "metric";
     String api = "497c54db9e77b4b34a094c92658d9d20";
     String urlTemplate = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=%s&APPID=%s";
     String requestUrl;
-    // Instantiate the RequestQueue.
     RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        weather = new Weather();
         requestWeather();
     }
 
     private void requestWeather() {
         queue = Volley.newRequestQueue(this);
-        textView = findViewById(R.id.text);
         requestUrl = String.format(urlTemplate, city, weatherFormat, api);
-
-
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, requestUrl, null, new Response.Listener<JSONObject>() {
-
                     @Override
                     public void onResponse(JSONObject response) {
-                        System.out.println(response);
-                        textView.setText("Response: " + response.toString());
+                        extractWeatherData(response);
                     }
                 }, new Response.ErrorListener() {
-
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
-
                     }
                 });
         queue.add(jsonObjectRequest);
     }
+
+    private void extractWeatherData(JSONObject response) {
+        try {
+            JSONObject main = response.getJSONObject("main");
+            weather.setTemp(main.getString("temp"));
+            weather.setHumidity(main.getString("humidity"));
+
+            JSONArray jArr = response.getJSONArray("weather");
+            JSONObject weatherDescription = jArr.getJSONObject(0);
+            weather.setDescription(weatherDescription.getString("description"));
+
+            JSONObject windDescription = response.getJSONObject("wind");
+            weather.setWindSpeed(windDescription.getString("speed"));
+
+            weather.setCity(response.getString("name"));
+            insertWeatherData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertWeatherData(){
+
+        TextView cityLabel = findViewById(R.id.cityName);
+        TextView tempValue = findViewById(R.id.tempValue);
+        TextView humidValue = findViewById(R.id.humidityValue);
+        TextView descriptionValue = findViewById(R.id.descriptionValue);
+        TextView windValue = findViewById(R.id.windSpeedValue);
+
+        cityLabel.setText(weather.getCity());
+        tempValue.setText(weather.getTemp());
+        humidValue.setText(weather.getHumidity());
+        descriptionValue.setText(weather.getDescription());
+        windValue.setText(weather.getWindSpeed());
+    }
+
 }
--- app/src/main/java/com/hfad/openweatherapp/Weather.java
+++ app/src/main/java/com/hfad/openweatherapp/Weather.java
@@ -1,8 +1,57 @@
 package com.hfad.openweatherapp;
 
-/**
- * Created by donald on 19/03/2018.
- */
-
 public class Weather {
+    private String city,temp, description, humidity, windSpeed;
+
+    public Weather(){
+
+    }
+
+    public Weather(String jsonCity, String jsonTemp, String jsonDescription, String jsonHumidity, String jsonWindSpeed){
+        city = jsonCity;
+        temp = jsonTemp;
+        description = jsonDescription;
+        humidity = jsonHumidity;
+        windSpeed = jsonWindSpeed;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
 }
